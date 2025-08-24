85fa18c23682b5d1ab6ae456675c103b0313e8a5
Vimal Prakash <vimalprakash3322@gmail.com>
2025-08-24 11:26:28 +0530

    feat(api): minimal api intergrated

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const express = require("express");
 
 // connecting db 
 require("./src/db/mongo")
+const cors = require("cors");
+
+
 
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api", require("./src/routers"));
 app.listen(process.env.PORT || 5000, () =>
   console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
