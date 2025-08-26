1bb2077104323f8c710f3eee8f782f2c8317410e
Vimal Prakash <vimalprakash3322@gmail.com>
2025-08-26 19:47:02 +0530

    lucide added

--- server/index.js
+++ server/index.js
@@ -38,6 +38,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add this before your routes
+app.use((req, res, next) => {
+  res.setHeader('Referrer-Policy', 'no-referrer-when-downgrade'); 
+  // other options: 'origin', 'unsafe-url', 'same-origin'
+  next();
+});
+
 // ---------------------- ROUTES ---------------------- //
 app.use("/api", require("./src/routers"));
 
